0286d7e72fc59155c079df94404339c6ae2d1e67
Grigorii Horos <horosgrisa@gmail.com>
2021-10-24 18:25:19 +0300

    New wersion

--- zpm-readme.plugin.zsh
+++ zpm-readme.plugin.zsh
@@ -5,5 +5,12 @@ autoload -Uz zpm-readme
 _zpm_extend_commands+=('readme:Show README for plugin')
 
 function _zpm_readme_completion() {
-  _describe -t commands "zpm subcommand" zsh_loaded_plugins
+
+  local _zpm_plugins=()
+
+  for key value in ${(kv)_ZPM_plugins_full}; do
+    _zpm_plugins+=("${key}")
+  done
+
+  _describe -t commands "zpm subcommand" _zpm_plugins
 }
